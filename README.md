c81ab1b14cd7d4c0c585d5be6800eceb209e464b
Gert <fakename0000000001@gmail.com>
2024-07-30 20:19:01 +0300

    cleaned code, updated README, changed DB authentication to get values from .env file

--- README.md
+++ README.md
@@ -1,40 +1,34 @@
-# Raintree_homework
+# RAINTREE PHP + MYSQL homework
+
+in this project I followed the instructions to demonstrate my skills using PHP and MYSQL
 
 
-sudo mysql -u root -p
+## Setting Up Your Local Environment
+
+You will need to have [MySQL](https://dev.mysql.com/doc/mysql-getting-started/en/) and [PHP](https://www.php.net/manual/en/install.php) installed and configured.
+
+1. Clone the repository.
+2. Create a `.env` file in the root directory of your project.
+3. Populate the `.env` file with your database credentials:
+```bash
+DB_SERVER=your_server_name
+DB_USERNAME=your_username
+DB_PASSWORD=your_password
+```
+4. build the database by:
+    * opening the mysql console while in the root directory
+    ```bash
+    mysql
+    ```
+    * running the following command:
+    ```bash
+    source setup.sql 
+    ```
+Now you are all set to test out the programm.
+
+ # HOW TO RUN:
+
+ ## Exercise 1:
 
 
-# Prerequistes:
 
-* MYSQL:
-    * Create USER 'gert'@'localhost'
-    * GRANT ALL PRIVILEGES ON *.* TO 'gert'@'localhost';
-
-
-
-mysql -u username -p database_name < setup.sql
-mysql -u gert -p insurance_db < setup.sql
-
-
-in order to run the setup.sql you will need to :
-* have access to your mysql console.
-* go into project root 
-* in mysql terminal -->:
-source setup.sql 
-
-
-
-DB commands: 
-
-source drop_all_table.sql
-
-
-use insurance_db
-
- show databases;
- show tables;
- describe patient;
- describe insurance;
-
- select * from patient;
- select * from insurance;
--- script.php
+++ script.php
@@ -1,61 +1,37 @@
-<!-- 3.2 The script has to have the following functionality:
+<?php
+$env = parse_ini_file('.env');
 
-a) Display the following columns for each patient to the console:
+$server_name = $env['DB_SERVER'];
+$username = $env['DB_USERNAME'];
+$password = $env['DB_PASSWORD'];
 
-Patient number (pn),
- Patient last name (last),
- Patient first name (first),
- Insurance name,
- Insurance - from date (from_date) in US short date format (MM-DD-YY) and Insurance to date (to_date) in US
-short date format (MM-DD-YY)
-
-, ordered by Insurance from date starting from earliest and then
-patient last name. -->
-
-<?php 
-$server_name = "localhost";
-$username = "gert";
-$password = "";
-
-$db_name = "insurance_db";
-$use_current_DB= "USE $db_name";
+$DB_NAME = "insurance_db";
+$use_current_DB= "USE $DB_NAME";
 
 
 $conn = mysqli_connect($server_name, $username, $password);
-
-
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error() . PHP_EOL);
 }
-echo "Connected successfully" . PHP_EOL;
+echo "DB Connected successfully" . PHP_EOL;
 
 
-// use current db
 $sql = "$use_current_DB";
 if (mysqli_query($conn, $sql)) {
-    echo "USE statment success"  . PHP_EOL;
 } else {
-    echo "Error creating database: " . mysqli_error($conn)  . PHP_EOL;
+    echo "Error with DB: " . mysqli_error($conn)  . PHP_EOL;
 }
 
-// SELECT LPAD(CAST(padded_value AS CHAR), 10, '0') AS formatted_value FROM example_table;
 
-// 3.1 B)
+echo "\n" . "solution to: 3.2 A):\n\n";
 
-
-// $sql = "SELECT _id, LPAD(CAST(pn AS CHAR), 10, '0') AS formatted_pn, first, last, dob FROM patient
 $sql = "SELECT  LPAD(CAST(p.pn AS CHAR), 10, '0') AS formatted_pn,  p.last,  p.first, p._id, i.from_date, i.to_date  FROM patient as p
 INNER JOIN insurance as i on i.patient_id = p._id
-
 ORDER BY to_date, last";
-$result = $conn->query($sql);
 
+$result = $conn->query($sql);
 if ($result->num_rows > 0) {
-    // output data of each row
     while($row = $result->fetch_assoc()) {
-        // _id | pn          | first   | last     | dob        |
-    //   echo "id: " . $row["_id"]. " - Patient number: " . $row["formatted_pn"]. " " . $row["first"]. $row["last"] . " " .  $row["dob"] . PHP_EOL;
       echo  $row["formatted_pn"] . ", ".$row["last"] . ", ". $row["first"] . ", ".  $row["from_date"] . "," .  $row["to_date"] . PHP_EOL;
     }
   } else {
@@ -63,8 +39,7 @@ if ($result->num_rows > 0) {
   }
 
 
-// 3.2 B)
-//  figure out how many letters in first an last names:
+echo "\n\n" . "solution to: 3.2 B):\n\n";
 
 $sql = "SELECT  p.last,  p.first FROM patient as p;";
 $result = $conn->query($sql);
@@ -74,7 +49,6 @@ $total = 0;
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo  $row["first"] . $row["last"] . PHP_EOL;
         
         $firstname_characters = str_split(strtoupper($row["first"])) ;
         $lastname_characters = str_split(strtoupper($row["last"]));
@@ -101,15 +75,13 @@ if ($result->num_rows > 0) {
   } else {
     echo "0 results";
   }
+
   ksort($letter_map);
 
-// TODO: sort alphabetically
   foreach ($letter_map as $key => $value) {
     $precentage = round($value/$total *100, 2);
     echo "$key\t$value\t$precentage" . "%\n";
-
 }
-echo $total . "\n";
 
 mysqli_close($conn);
 
